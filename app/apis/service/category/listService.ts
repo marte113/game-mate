19913fc2a67f2a30c56be9bb5bc155832c046af1
marte113 <qwerty8739987@gmail.com>
2025-09-02 17:35:01 +0900

    [refactor] : listService.ts base레이어 pagination 계산 함수 활용

--- app/apis/service/category/listService.ts
+++ app/apis/service/category/listService.ts
@@ -1,7 +1,7 @@
 'use server'
 import { listGames } from '@/app/apis/repository/category/gamesRepository'
 
-import { calcLimitOffset } from '../../base/pagination';
+import { calcLimitOffset, calcNextPage } from '../../base/pagination';
 
 const INITIAL_LOAD = 18
 const LOAD_PER_PAGE = 12
@@ -12,10 +12,8 @@ export async function getCategoryList(page: number) {
 
   const {limit, offset} = calcLimitOffset({page, initialLoad : INITIAL_LOAD, perPage : LOAD_PER_PAGE});
 
-  
   const games = await listGames(offset, limit)
-  const hasNextPage = (games?.length ?? 0) === limit
-  return { games: games ?? [], nextPage: hasNextPage ? page + 1 : undefined }
+  const hasMore = (games?.length ?? 0) === limit
+  const nextPage = calcNextPage(hasMore, page)
+  return { games: games ?? [], nextPage }
 }
-
-
