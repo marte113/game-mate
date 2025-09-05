f0f0e0c41cd1547bb93378bbee35bac18e614da2
marte113 <qwerty8739987@gmail.com>
2025-09-06 04:30:52 +0900

    [fix] : task/page.tsx 동적 페이지화

--- app/dashboard/task/page.tsx
+++ app/dashboard/task/page.tsx
@@ -1,10 +1,12 @@
-import { Suspense } from "react";
+import { Suspense } from "react"
 
-import TaskList from "./_components/TaskList";
-import TaskPageContainer from "./_components/TaskPageContainer";
-import TaskPageNavTab from "./_components/TaskPageNavTab";
-import TaskPageNavTabSkeleton from "./_components/skeletons/TaskPageNavTabSkeleton";
-import TaskListSkeleton from "./_components/skeletons/TaskListSkeleton";
+import TaskList from "./_components/TaskList"
+import TaskPageContainer from "./_components/TaskPageContainer"
+import TaskPageNavTab from "./_components/TaskPageNavTab"
+import TaskPageNavTabSkeleton from "./_components/skeletons/TaskPageNavTabSkeleton"
+import TaskListSkeleton from "./_components/skeletons/TaskListSkeleton"
+
+export const dynamic = "force-dynamic"
 
 export default function TaskPage() {
   return (
@@ -17,5 +19,5 @@ export default function TaskPage() {
         <TaskList />
       </Suspense>
     </TaskPageContainer>
-  );
+  )
 }
